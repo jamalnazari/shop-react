15e0d1c67d02337bae5e020167516fd171c2a615
jamalnazari <jamalnazari15@gmail.com>
2025-11-28 00:47:32 +0330

    PRODUCTS
    تصویر محصولات طور یدر آبجکتها لینک شد که بتوان در لینک دمو آنها را مشاهده کرد

--- src/data/products.jsx
+++ src/data/products.jsx
@@ -1,168 +1,31 @@
-import A06 from '/A06.jpg'
-import A56 from '/A56.jpg'
-import A24 from '/A24.jpg'
-import A34 from '/A34.jpg'
-import A14 from '/A14.jpg'
-import A07 from '/A07.jpg'
-import A36 from '/A36.jpg'
-import A12 from '/A12.jpg'
-import iphon11 from '/iphon11.jpg'
-import iphon11pro from '/iphon11pro.jpg'
-import iphon12 from '/iphon12.jpg'
-import iphon12pro from '/iphon12pro.jpg'
-import iphon13 from '/iphon13.jpg'
-import iphon13pro from '/iphon13pro.jpg'
-import iphon14pro from '/iphon14pro.jpg'
-import iphon14 from '/iphon14.jpg'
-import iphon15 from '/iphon15.jpg'
-import iphon15pro from '/iphon15pro.jpg'
-import iphon16 from '/iphon16.jpg'
-import not6 from '/not6.jpg'
-import not8 from '/not8.jpg'
-import not9 from '/not9.jpg'
-import not13 from '/not13.jpg'
-import not14 from '/not14.jpg'
-
-
 export const PRODUCTS = [
-   {
-     id : 1 , 
-     name : 'A06' , 
-     price : 8000000 ,
-     productimg : A06
-   },
-   {
-     id : 2 , 
-     name : 'A56' , 
-     price : 7500000 ,
-     productimg : A56
-   },
-   {
-     id : 3 , 
-     name : 'A24' ,  
-     price : 58000000 ,
-     productimg: A24
-   },
-   {
-     id : 4, 
-     name : '34' , 
-     price : 24000000 ,
-     productimg : A34
-   },
-   {
-     id : 5 , 
-     name : 'A12' , 
-     price : 6000000,
-     productimg : A12
-   },
-   {
-     id : 6 , 
-     name : 'A07' , 
-     price : 4500000,
-     productimg : A07
-   },
-   {
-     id : 7 , 
-     name : 'A36' , 
-     price : 7200000,
-     productimg : A36
-   },
-   {
-     id : 8 , 
-     name : 'iphon12' , 
-     price : 94600000,
-     productimg : iphon12
-   },
-   {
-     id : 9 , 
-     name : 'iphon12pro' , 
-     price : 640000000,
-     productimg : iphon12pro
-   },
-   {
-     id : 10 , 
-     name : 'iphon13' , 
-     price : 290000000,
-     productimg : iphon13
-   },
-   {
-     id : 12 , 
-     name : 'iphon13pro' , 
-     price : 73000000,
-     productimg : iphon13pro
-   },
-   {
-     id : 11 , 
-     name : 'iphon14' , 
-     price : 63000000,
-     productimg : iphon14
-   },
-   
-   {
-     id : 13 , 
-     name : 'iphon14pro' , 
-     price : 51000000,
-     productimg : iphon14pro
-   },
-   {
-     id : 14 , 
-     name : 'iphon15' , 
-     price : 175000000,
-     productimg : iphon15
-   },
-   {
-     id : 15 , 
-     name : 'iphon15pro' , 
-     price : 92000000,
-     productimg : iphon15pro
-   },
-   {
-     id : 16 , 
-     name : 'iphon16' , 
-     price : 270000000,
-     productimg : iphon16
-   },
-   {
-     id : 17 , 
-     name : 'iphon11' , 
-     price : 47000000,
-     productimg : iphon11
-   },
-   {
-     id : 18 , 
-     name : 'iphon11pro' , 
-     price : 48000000,
-     productimg : iphon11pro
-   },
-   {
-     id : 19, 
-     name : 'not6' , 
-     price : 3200000,
-     productimg : not6
-   },
-   {
-     id : 20, 
-     name : 'not8' , 
-     price : 8300000,
-     productimg : not8
-   },
-   {
-     id : 21, 
-     name : 'not9' , 
-     price : 9300000,
-     productimg : not9
-   },
-   {
-     id : 22, 
-     name : 'not13' , 
-     price : 8600000,
-     productimg :not13 
-   },
-   {
-     id : 23, 
-     name : 'not14' , 
-     price : 24000000,
-     productimg : not14
-   }
-]
+  { id: 1, name: "A06", price: 8000000, productimg: "/A06.jpg" },
+  { id: 2, name: "A56", price: 7500000, productimg: "/A56.jpg" },
+  { id: 3, name: "A24", price: 58000000, productimg: "/A24.jpg" },
+  { id: 4, name: "A34", price: 24000000, productimg: "/A34.jpg" },
+  { id: 5, name: "A12", price: 6000000, productimg: "/A12.jpg" },
+  { id: 6, name: "A07", price: 4500000, productimg: "/A07.jpg" },
+  { id: 7, name: "A36", price: 7200000, productimg: "/A36.jpg" },
+  
+  { id: 8, name: "iphon12", price: 94600000, productimg: "/iphon12.jpg" },
+  { id: 9, name: "iphon12pro", price: 640000000, productimg: "/iphon12pro.jpg" },
 
+  { id: 10, name: "iphon13", price: 290000000, productimg: "/iphon13.jpg" },
+  { id: 12, name: "iphon13pro", price: 73000000, productimg: "/iphon13pro.jpg" },
+
+  { id: 11, name: "iphon14", price: 63000000, productimg: "/iphon14.jpg" },
+  { id: 13, name: "iphon14pro", price: 51000000, productimg: "/iphon14pro.jpg" },
+
+  { id: 14, name: "iphon15", price: 175000000, productimg: "/iphon15.jpg" },
+  { id: 15, name: "iphon15pro", price: 92000000, productimg: "/iphon15pro.jpg" },
+
+  { id: 16, name: "iphon16", price: 270000000, productimg: "/iphon16.jpg" },
+  { id: 17, name: "iphon11", price: 47000000, productimg: "/iphon11.jpg" },
+  { id: 18, name: "iphon11pro", price: 48000000, productimg: "/iphon11pro.jpg" },
+
+  { id: 19, name: "not6", price: 3200000, productimg: "/not6.jpg" },
+  { id: 20, name: "not8", price: 8300000, productimg: "/not8.jpg" },
+  { id: 21, name: "not9", price: 9300000, productimg: "/not9.jpg" },
+  { id: 22, name: "not13", price: 8600000, productimg: "/not13.jpg" },
+  { id: 23, name: "not14", price: 24000000, productimg: "/not14.jpg" },
+];
