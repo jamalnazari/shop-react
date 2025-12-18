ad156f4ae80c0d7392f242cea289eaa1d39131af
jamalnazari <jamalnazari15@gmail.com>
2025-12-18 17:02:59 +0330

    ساخت پروکسی برای درخواست به سرور

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/shop-react/',  // اسم ریپوی گیتهاب
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://dummyjson.com',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
